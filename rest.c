3c1e222289e8c90c9fa6910a84f1023d20ab682c
Connor Milligan <connoramilligan@gmail.com>
2022-03-01 19:38:26 -0500

    used a writer func from the libcurl docs

--- rest.c
+++ rest.c
@@ -1,41 +1,31 @@
 #include "rest.h"
 
-struct string {
-  char *ptr;
-  size_t len;
-};
-
-void init_string(struct string *s) {
-  s->len = 0;
-  s->ptr = malloc(s->len+1);
-  if (s->ptr == NULL) {
-    fprintf(stderr, "malloc() failed\n");
-    exit(EXIT_FAILURE);
-  }
-  s->ptr[0] = '\0';
-}
-
-size_t writefunc(void *ptr, size_t size, size_t nmemb, struct string *s)
-{
-  size_t new_len = s->len + size*nmemb;
-  s->ptr = realloc(s->ptr, new_len+1);
-  if (s->ptr == NULL) {
-    fprintf(stderr, "realloc() failed\n");
-    exit(EXIT_FAILURE);
-  }
-  memcpy(s->ptr+s->len, ptr, size*nmemb);
-  s->ptr[new_len] = '\0';
-  s->len = new_len;
-
-  return size*nmemb;
-}
+ struct memory {
+   char *response;
+   size_t size;
+ };
+ 
+ static size_t cb(void *data, size_t size, size_t nmemb, void *userp)
+ {
+    size_t realsize = size * nmemb;
+    struct memory *mem = (struct memory *)userp;
+ 
+    char *ptr = realloc(mem->response, mem->size + realsize + 1);
+    if(ptr == NULL)
+        return 0;  /* out of memory! */
+ 
+    mem->response = ptr;
+    memcpy(&(mem->response[mem->size]), data, realsize);
+    mem->size += realsize;
+    mem->response[mem->size] = 0;
+  
+    return realsize;
+ }
 
 void RESTget(char *rest) {
     CURL *curl;
     CURLcode res;
-    struct string s;
-    init_string(&s);
-    
+    struct memory chunk = {0};
 
     curl = curl_easy_init();
     if(curl) {
@@ -43,17 +33,16 @@ void RESTget(char *rest) {
         /* example.com is redirected, so we tell libcurl to follow redirection */
         curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
         /* Perform the request, res will get the return code */
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, cb);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
         res = curl_easy_perform(curl);
         /* Check for errors */
         
         if(res != CURLE_OK)
             fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        rest = (char *) realloc(rest, chunk.size);
+        strcpy(rest, chunk.response);
 
-        //rest = (char *) realloc(rest, sizeof(char)*s.len);
-        strcpy(rest, s.ptr);
-        free(s.ptr);
 
         /* always cleanup */
         curl_easy_cleanup(curl);
